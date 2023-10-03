a908204e4a193fb3c3ad3cbb245e750de24061a4
gamalielara <aragamalielboanerges@gmail.com>
2023-10-03 16:35:07 +0700

    add webpack config

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Toast: React.FC = () => {
+  return <div className={styles["toast-wrapper"]}>HEI</div>;
+};
+
+export default Toast;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      name: "bubble-gum",
+      type: "umd",
+    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
