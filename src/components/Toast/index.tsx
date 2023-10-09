663c38278395bc92afb840c0f9bfca4804515247
gamalielara <aragamalielboanerges@gmail.com>
2023-10-09 18:40:11 +0700

    add createportal

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./index.module.scss";
+import {createPortal} from "react-dom";
 
 const Toast: React.FC = () => {
-  return <div className={styles["toast-wrapper"]}>hei hei heyy hellow</div>;
+  return createPortal(<div className={styles["toast-wrapper"]}>hei hei heyy hellow</div>, document.body);
 };
 
-export default Toast;
+export default Toast
+
